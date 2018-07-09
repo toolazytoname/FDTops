2533bf1d3e6a3f10ce6772c48f60c5ab81847a1e
weichao <weichao@yiche.com>
2018-07-09 20:39:29 +0800

    initial

---
+++ FDTops.py
@@ -0,0 +1,162 @@
+import os,sys
+
+def obtan_dir_names(work_dir):
+    '''
+
+    :param work_dir: 目标目录
+    :return:return:根据文件目录集合生成一个字符串
+    '''
+    parent_set = set()
+    for parent, dirnames, filenames in os.walk(work_dir):
+        if parent.endswith('/'):
+            parent_set.add('{0}*'.format(parent))
+        else:
+            parent_set.add('{0}/*'.format(parent))
+    parent_string = ' '.join(str(parent) for parent in parent_set)
+    return parent_string
+
+
+def obtain_class_name_set(work_dir):
+    '''
+    获取代操作类名的集合
+    :param work_dir:目标目录
+    :return:  获取代操作类名的集合
+    '''
+    class_name_set = set()
+    for parent, dirnames, filenames in os.walk(work_dir):
+        for filename in filenames:
+            if filename.endswith('.h'):
+                (shortname, extension) = os.path.splitext(filename)
+                class_name_set.add(shortname)
+    return class_name_set
+
+def obtain_class_rename_dic(operation, prefix, des_prefix, class_name_set):
+    '''
+    获取一个字典，key为原始类名，value 为目标类名
+    :param operation:操作符，目前支持 replace  和 add
+    :param prefix: 前缀
+    :param des_prefix:替换的目标前缀
+    :param class_name_set: 类名集合
+    :return: 一个字典，key为原始类名，value 为目标类名
+    '''
+    class_rename_dic = dict()
+    for class_name in class_name_set:
+        if 'add' == operation:
+            class_rename_dic[class_name] = '{0}{1}'.format(prefix, class_name)
+        elif 'replace' == operation:
+            class_rename_dic[class_name] = class_name.replace(prefix, des_prefix)
+    return class_rename_dic
+
+
+def save_tops_file(work_dir, class_rename_dic):
+    '''
+    以传入的目标目录名为文件名，维护一个tops替换规则文件
+    :param work_dir:工作目录
+    :param operation: 操作符，分add 和 replace 两种
+    :param prefix: 前缀
+    :return:  以传入的目标目录名为文件名，维护一个tops替换规则文件
+    '''
+    (work_dir_dirname, work_dir_filename) = os.path.split(work_dir)
+    tops_line_set = set()
+    # for class_name, des_class_name in class_rename_dic.items():
+    tops_line_set = {'replace "{0}" with "{1}"'.format(key, value)for key, value in class_rename_dic.items()}
+
+    # print('tops_line_set is {0}'.format(tops_line_set))
+    # print('work_dir_dirname is {0}'.format(work_dir_filename))
+    with open('{0}.tops'.format(work_dir_filename), mode='w', encoding='utf-8') as a_file:
+        for tops_line in tops_line_set:
+            a_file.writelines(tops_line)
+            a_file.write('\n')
+
+
+def run_shell(work_dir, parentString):
+    '''
+    根据传入的参数执行tops 命令行工具
+    :param work_dir:目标目录
+    :param parentString: 目标目录
+    :return:
+    '''
+    (work_dir_dirname, work_dir_filename) = os.path.split(work_dir)
+    shellString = 'tops -semiverbose -scriptfile ./{0}.tops  {1}'.format(work_dir_filename, parentString)
+    print('shellString:{0}'.format(shellString))
+    os.system(shellString)
+
+def obtain_file_path_set(work_dir):
+    '''
+    获取待操作文件的集合
+    :param work_dir:目标目录
+    :return: 待操作文件集合
+    '''
+    file_path_set = set()
+    for parent, dirnames, filenames in os.walk(work_dir):
+        for filename in filenames:
+            if filename.endswith('.h') or filename.endswith('.m'):
+                file_path = os.path.join(parent, filename)
+                file_path_set.add(file_path)
+    return file_path_set
+
+
+def rename_class_handle(class_rename_dic, file_path_set):
+    '''
+    手动操作文件中的内容
+
+    :param class_rename_dic:
+    :param file_path_set:
+    :return:
+    '''
+    for file_path in file_path_set:
+        with open(file_path, "r+") as file:
+            read_data = file.read()
+            file.seek(0, 0)
+            # result_data = ''
+            for key, value in class_rename_dic.items():
+                original_string = '"{0}.h"'.format(key)
+                des_string = '"{0}.h"'.format(value)
+                read_data = read_data.replace(original_string, des_string)
+            file.write(read_data)
+
+def obtain_filepath_dic(class_rename_dic, file_path_set):
+    '''
+    返回 原始文件名 和 目标文件名 映射的字典
+    :param class_rename_dic:原始类，目标类名映射关系的字典
+    :param file_path_set:原始文件名的集合
+    :return:返回key为原始文件名，value为目标文件名 映射的字典
+    '''
+    file_path_dic = dict()
+    for file_path in file_path_set:
+        (file_path_dirname, file_path_fileName) = os.path.split(file_path)
+        (shortname, extension) = os.path.splitext(file_path_fileName)
+        new_file_name = '{0}{1}'.format(class_rename_dic[shortname], extension)
+        new_file_path = os.path.join(file_path_dirname, new_file_name)
+        file_path_dic[file_path] = new_file_path
+    return file_path_dic
+
+def os_rename(file_path_dic):
+    '''
+    批量重命名文件
+    :param file_path_dic:key为原始文件名，value为目标文件名
+    :return:
+    '''
+    {os.rename(oldFilePath, newFilePath) for oldFilePath, newFilePath in file_path_dic.items()}
+
+
+
+if __name__=='__main__':
+    work_dir = sys.argv[1]
+    operation = sys.argv[2]
+    prefix = sys.argv[3]
+    des_prefix = ''
+    if sys.argv.__len__() >= 5:
+        des_prefix = sys.argv[4]
+
+    dir_names = obtan_dir_names(work_dir)
+    class_name_set = obtain_class_name_set(work_dir)
+    class_rename_dic = obtain_class_rename_dic(operation, prefix, des_prefix, class_name_set)
+    save_tops_file(work_dir, class_rename_dic)
+    run_shell(work_dir, dir_names)
+
+    file_path_set = obtain_file_path_set(work_dir)
+    file_path_dic = obtain_filepath_dic(class_rename_dic, file_path_set)
+    rename_class_handle(class_rename_dic,file_path_set)
+
+    os_rename(file_path_dic)
